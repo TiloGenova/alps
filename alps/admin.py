cef2eb1ed44f97bcb5615c57c09233abb7df4631
TiloGenova <tilo.oschatz@googlemail.com>
2020-11-22 11:54:17 +0100

    Models created

--- alps/admin.py
+++ alps/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Activity
+from .models import Contact
+
+
 
 # Register your models here.
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('category',)
+
+
+admin.site.register(Activity)
+admin.site.register(Contact)
--- alps/models.py
+++ alps/models.py
@@ -1,3 +1,39 @@
+from __future__ import unicode_literals
 from django.db import models
 
+
 # Create your models here.
+class Activity(models.Model):
+    CHOICES = (
+        ('sport', 'Sport'),
+        ('luoghi', "Luoghi d'interesse"),
+        ('piatti', 'Piatti tipici'),
+        ('after ski', 'After Ski'),
+        ('cena', 'Cena in quota'),
+    )
+
+    title = models.CharField(max_length=150)
+    category = models.CharField(max_length=150, null=False, choices=CHOICES)
+    discription = models.CharField(max_length=350)
+    text = models.TextField(blank=True)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=150)
+    street = models.CharField(max_length=150)
+    phone = models.CharField(max_length=150)
+    partitaiva = models.CharField(max_length=150)
+    email = models.CharField(max_length=150)
+    homepage = models.CharField(max_length=150, blank=True)
+
+
+    emailbluchalet = models.CharField(max_length=150)
+    phonebluchalet = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
