23176e52fb267d787ad4874a9570c9f95e1e0b21
TiloGenova <tilo.oschatz@googlemail.com>
2020-11-28 08:53:36 +0100

    Detail page to correct

--- alps/urls.py
+++ alps/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from alps import views
 
+app_name ='alps'
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('sports/', views.sports, name='sports'),
     path('contatti/', views.contatti, name='contatti'),
+    path('luoghi/', views.luoghi, name='places'),
+    path('alps/<int:activity_id>', views.details, name='details'),
 
 
 
--- alps/views.py
+++ alps/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Activity, Contact
 
 # Create your views here.
+
+
 def home(request):
     contactall = get_object_or_404(Contact, pk=1)
     activities = Activity.objects.all()
@@ -12,8 +14,6 @@ def home(request):
     return render(request, 'alps/home.html', {'contactall': contactall, 'activities': activities,
                                               'sport_activities': sport_activities,
                                               'enos': enos, 'places': places})
-
-
 
 
 def base(request):
@@ -28,9 +28,24 @@ def contatti(request):
 def sports(request):
     contactall = get_object_or_404(Contact, pk=1)
     sport_activities = Activity.objects.filter(category='sport')
-    #sport_activities = Activity.objects.get(category='sport')   -- GET only for one object
+    # sport_activities = Activity.objects.get(category='sport')   -- GET only for one object
     enos = Activity.objects.filter(category='after ski')[:2]
     places = Activity.objects.filter(category='luoghi')[:2]
     return render(request, 'alps/sports.html', {'contactall': contactall, 'sport_activities': sport_activities,
                                                 'enos': enos, 'places': places})
 
+
+def luoghi(request):
+    contactall = get_object_or_404(Contact, pk=1)
+    sport_activities = Activity.objects.filter(category='sport')[:2]
+    enos = Activity.objects.filter(category='after ski')[:2]
+    places = Activity.objects.filter(category='luoghi')
+    return render(request, 'alps/luoghi.html', {'contactall': contactall, 'sport_activities': sport_activities,
+                                                'enos': enos, 'places': places})
+
+
+def details(request, activity_id):
+    contactall = get_object_or_404(Contact, pk=1)
+    activities = Activity.objects.all()
+    activity_id = get_object_or_404(Activity, pk=activity_id)
+    return render(request, 'alps/details.html', {'contactall': contactall, 'id': activity_id})
